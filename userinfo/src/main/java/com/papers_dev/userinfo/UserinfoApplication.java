5770e32bc2753d2d3cf74e3c66b5e14bd52e7b06
SeleneDaza <arian.daza@uptc.edu.co>
2025-10-21 11:40:48 -0500

    carpetas

---
+++ userinfo/src/main/java/com/papers_dev/userinfo/UserinfoApplication.java
@@ -0,0 +1,13 @@
+package com.papers_dev.userinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserinfoApplication.class, args);
+	}
+
+}
---
+++ userinfo/src/test/java/com/papers_dev/userinfo/UserinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papers_dev.userinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
